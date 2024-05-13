33d5541d794169effe8167a4acee1acee7d1d0ce
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-13 14:46:59 -0400

    last of the testing

--- music.sql
+++ music.sql
@@ -19,11 +19,115 @@
 -- JOIN Genre ON Track.GenreId = Genre.GenreId
 -- WHERE Artist.Name = 'Nirvana'
 
-SELECT Track.Name AS Track,
-Album.Title AS Album,
-Artist.Name AS Artist,
-Genre.Name AS Genre
+
+-- SELECT Track.Name AS Track,
+-- Album.Title AS Album,
+-- Artist.Name AS Artist,
+-- Genre.Name AS Genre
+-- FROM Track
+-- JOIN Album ON Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN Genre ON Track.GenreId = Genre.GenreId
+-- WHERE Album.Title = 'Nevermind'
+
+
+-- SELECT Track.Name AS Track,
+-- Album.Title AS Album,
+-- Artist.Name AS Artist
+-- Genre.Name AS Genre
+-- FROM Track
+-- JOIN Album ON Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN Genre ON  Track.GenreId = Genre.GenreId
+-- WHERE Track.Name LIKE '%Dog%'
+
+
+-- SELECT Track.Name AS Track,
+-- Album.Title AS Album,
+-- Artist.Name AS Artist,
+-- Genre.Name AS Genre
+-- FROM Track
+-- JOIN Album ON Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN Genre ON Track.GenreId = Genre.GenreId
+-- WHERE Track.Name LIKE '%Dog%'
+-- AND Artist.Name = 'Led Zeppelin'
+
+
+-- SELECT * FROM Playlist;
+-- SELECT * FROM PlaylistTrack;
+
+-- SELECT Playlist.Name as Playlist, Track.Name As Track, Artist.Name AS Artist
+-- FROM Track
+-- JOIN Album on Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId
+-- JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+-- WHERE Playlist.Name = 'Grunge'
+
+-- SELECT Playlist.Name as Playlist, Track.Name As Track, Artist.Name AS Artist
+-- FROM Track
+-- JOIN Album on Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId
+-- JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+-- WHERE Artist.Name = 'Nirvana'
+-- ORDER BY Playlist.Name
+
+-- SELECT Playlist.Name as Playlist, 
+-- Track.Name As Track, 
+-- Artist.Name AS Artist
+-- FROM Track
+-- JOIN Album on Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId
+-- JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+-- WHERE Artist.Name = 'Nirvana'
+-- ORDER BY Track.Name
+
+
+
+-- SELECT Playlist.Name as Playlist,
+-- Track.Name As Track,
+-- Artist.Name as Artist,
+-- Album.Title as Title
+-- FROM Track
+-- JOIN Album ON Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId
+-- JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+-- WHERE Artist.Name = 'Nirvana'
+-- GROUP BY Playlist.Name, Track.Name
+-- ORDER BY Playlist.Name, Track.Name
+
+
+-- SELECT COUNT(Playlist.Name) as PlaylistAppearances,
+-- Track.Name As Track,
+-- Artist.Name as Artist,
+-- FROM Track
+-- JOIN Album ON Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId
+-- JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+-- WHERE Artist.Name = 'Nirvana'
+-- GROUP BY Track.Name
+-- ORDER BY PlaylistAppearances DESC
+
+-- SELECT COUNT(Playlist.Name) as PlaylistAppearances,
+-- Track.Name As Track,
+-- Artist.Name as Artist
+-- FROM Track
+-- JOIN Album ON Track.AlbumId = Album.AlbumId
+-- JOIN Artist ON Album.ArtistId = Artist.ArtistId
+-- JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId
+-- JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+-- WHERE Artist.Name = 'Nirvana'
+-- GROUP BY Track.Name
+-- ORDER BY PlaylistAppearances DESC
+
+SELECT SUM(Track.UnitPrice) as TotalCost,
+Playlist.Name as Playlist
 FROM Track
-JOIN Album ON Track.AlbumId = Album.AlbumId
-JOIN Artist ON Album.ArtistId = Artist.ArtistId
-JOIN Genre ON Track.GenreId = Genre.GenreId
+JOIN PlaylistTrack ON Track.TrackId - PlaylistTrack.TrackId
+jOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+GROUP BY Playlist.Name
